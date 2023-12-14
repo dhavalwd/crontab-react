c726edf2842a398214c2b220d9c224af4ec842e2
Dhaval Vyas <dhavs1989@gmail.com>
2023-12-14 16:43:10 -0400

    upgrade storybook, upgrade stories

--- src/stories/Crontab.stories.js
+++ src/stories/Crontab.stories.js
@@ -4,11 +4,33 @@ import Crontab from "../Crontab";
 export default {
   title: "Example/Crontab",
   component: Crontab,
-  tags: ["autodocs"],
+  argTypes: {
+    shortSelectedOptions: { control: "boolean" },
+    invalidCronStringErrorMessage: { control: "text" },
+    value: { control: "text" },
+  },
+};
+
+export const Default = {
+  args: {},
+};
+
+export const PreSelectedValue = {
+  args: {
+    value: "1-6 1-6 1-3 1-2,7 1,6",
+  },
 };
 
 export const ShowLongName = {
   args: {
     shortSelectedOptions: false,
+    value: "* * * 1-2 2-4",
+  },
+};
+
+export const InvalidErrorMessage = {
+  args: {
+    invalidCronStringErrorMessage: "Oops. Invalid cron string",
+    value: "aaaa",
   },
 };
